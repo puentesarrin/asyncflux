52be74dff00b1321c9d48de6513b9533db0fa0d3
Jorge Puente Sarrín <puentesarrin@gmail.com>
2014-05-28 05:30:10 +0000

    Added BaseUsers class to support different user levels.

--- asyncflux/clusteradmins.py
+++ asyncflux/clusteradmins.py
@@ -1,40 +1,12 @@
 # -*- coding: utf-8 -*-
 """Tools for cluster administration"""
-from tornado import gen
-
-from asyncflux.util import asyncflux_coroutine
+from asyncflux.common import BaseUsers
 
 
-class ClusterAdmins(object):
+class ClusterAdmins(BaseUsers):
 
-    def __init__(self, client):
-        self.__client = client
-
-    @property
-    def client(self):
-        return self.__client
-
-    @asyncflux_coroutine
-    def get_all(self):
-        users = yield self.client.request('/cluster_admins')
-        raise gen.Return([u['username'] for u in users])
-
-    @asyncflux_coroutine
-    def add(self, username, password):
-        yield self.client.request('/cluster_admins', method='POST',
-                                  body={'name': username, 'password': password})
-
-    @asyncflux_coroutine
-    def update(self, username, new_password):
-        yield self.client.request('/cluster_admins/%(username)s',
-                                  path_params={'username': username},
-                                  body={'password': new_password},
-                                  method='POST')
-
-    @asyncflux_coroutine
-    def delete(self, username):
-        yield self.client.request('/cluster_admins/%(username)s',
-                                  {'username': username}, method='DELETE')
+    def _get_path(self):
+        return '/cluster_admins'
 
     def __repr__(self):
         return 'ClusterAdmins(%r)' % self.client
---
+++ asyncflux/common.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*
+from tornado import gen
+
+from asyncflux.util import asyncflux_coroutine
+
+
+class BaseUsers(object):
+
+    def __init__(self, client):
+        self.__client = client
+
+    @property
+    def client(self):
+        return self.__client
+
+    def _get_path(self):
+        raise NotImplementedError
+
+    @asyncflux_coroutine
+    def get_all(self):
+        users = yield self.client.request(self._get_path())
+        raise gen.Return([u['username'] for u in users])
+
+    @asyncflux_coroutine
+    def add(self, username, password):
+        yield self.client.request(self._get_path(), method='POST',
+                                  body={'name': username, 'password': password})
+
+    @asyncflux_coroutine
+    def update(self, username, new_password):
+        yield self.client.request(self._get_path() + '/%(username)s',
+                                  path_params={'username': username},
+                                  body={'password': new_password},
+                                  method='POST')
+
+    @asyncflux_coroutine
+    def delete(self, username):
+        yield self.client.request(self._get_path() + '/%(username)s',
+                                  {'username': username}, method='DELETE')
