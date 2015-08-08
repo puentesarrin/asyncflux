0fab9bef0144fd648f9087f9cb307e82eb1bddfb
Jorge Puente-Sarrín <puentesarrin@gmail.com>
2015-08-07 20:47:23 -0500

    Removed errors modules, exception clasess from influxdb will eventually used.

--- asyncflux/errors.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class AsyncfluxError(Exception):
-
-    def __init__(self, http_response):
-        self.response = http_response
-        self.message = http_response.body
-        super(AsyncfluxError, self).__init__(self.message)
