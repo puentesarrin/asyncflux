6dfe0b5ce4bd35585ef989f952fd04fdbcdd05c2
Jorge Puente-Sarrín <puentesarrin@gmail.com>
2015-08-09 16:05:25 -0500

    Added Tornado docs as reference for Sphinx docs.

--- docs/conf.py
+++ docs/conf.py
@@ -270,4 +270,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/', None),
+    'tornado': ('http://www.tornadoweb.org/en/stable/', None),
+}
